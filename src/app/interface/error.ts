8ef6a1be92e8040dbeff74dd13b751b298fb3f5e
meskatabusayed <meskatabusayed@gmail.com>
2026-04-23 07:25:32 +0530

    error interface add

---
+++ src/app/interface/error.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+export type TErrorSources = {
+    path : number | string;
+    message : string;
+  }[];
+
+export type TGenericErrorResponse = {
+    statusCode : number;
+    message : string;
+    errorSources : TErrorSources
+}
