459ca7e2a771410290bbd753ec7816c01d211cb2
meskatabusayed <meskatabusayed@gmail.com>
2026-04-23 07:29:05 +0530

    cast error handle done

---
+++ src/app/error/handleCastError.ts
@@ -0,0 +1,26 @@
+/* eslint-disable import/order */
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/consistent-type-imports */
+import mongoose from 'mongoose';
+import { TErrorSources, TGenericErrorResponse } from '../interface/error';
+
+
+const handleCastError = (
+  err: mongoose.Error.CastError
+): TGenericErrorResponse => {
+  const errorSources: TErrorSources = [
+    {
+      path: err?.path,
+      message: err?.message,
+    },
+  ];
+
+  const statusCode = 400;
+  return {
+    statusCode,
+    message: 'Invalid ID',
+    errorSources,
+  };
+};
+
+export default handleCastError;
